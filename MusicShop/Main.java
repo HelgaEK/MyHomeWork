5b95e555b6e640edea8785ff10d560e8842c0240
Olga <mary.killer.ololo.yeah@gmail.com>
2016-02-23 18:47:12 +0200

    Update Main.java

--- MusicShop/Main.java
+++ MusicShop/Main.java
@@ -3,25 +3,25 @@ public class Main {
     public static void main(String[] args) {
 
         Shop musShop = new Shop();
-        musShop.setName("Bukvica");
-        musShop.setAddress("Ostafiya Dashkevich, 19");
-        musShop.setNumberBooksOrder(2);
+        musShop.setName("City Music");
+        musShop.setAddress("Shevchenko Boulevard,352");
+        musShop.setNumberInstrumentOrder(2);
 
-        Guitar guitar = new Guitar();
-        guitar.setName("Fahrenheit 451");
-        guitar.setAuthor("Ray Bradbury");
-        guitar.setCity("United States");
-        guitar.setPages(159);
-        guitar.setYearPublished(1953);
-        musShop.addGuitar(guitar);
+        Instrument instrument = new Instrument();
+        instrument.setType("bass guitar");
+        instrument.setBrand("Ibanez");
+        instrument.setManufacturer("Japan");
+        instrument.setNumberOfString (4);
+        instrument.setModel("GSR180 BK");
+        musShop.addInstrument(instrument);
 
-        guitar = new Guitar();
-        guitar.setName("The Master and Margarita");
-        guitar.setAuthor("Mikhail Bulgakov");
-        guitar.setCity("Soviet Union");
-        guitar.setPages(400);
-        guitar.setYearPublished(1967);
-        musShop.addGuitar(guitar);
+        Instrument instrument1 = new Instrument();
+        instrument1.setType("electro guitar");
+        instrument1.setBrand("Gibson");
+        instrument1.setManufacturer("USA");
+        instrument1.setNumberOfString (6);
+        instrument1.setModel("ES0335 TDC");
+        musShop.addInstrument(instrument);
 
         System.out.println(musShop.toString());
     }
