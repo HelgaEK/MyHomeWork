6ebfbf5e1422ecb2db27dc3f5413e5cad804f8b8
Olga <mary.killer.ololo.yeah@gmail.com>
2016-03-08 16:33:09 +0200

    Недороблений
    Частина коду закоментована, бо ще не опрацьована.

--- MusicShop/Main.java
+++ MusicShop/Main.java
@@ -1,4 +1,3 @@
-
 public class Main {
     public static void main(String[] args) {
 
@@ -7,22 +6,32 @@ public class Main {
         musShop.setAddress("Shevchenko Boulevard,352");
         musShop.setNumberInstrumentOrder(2);
 
-        Instrument instrument = new Instrument();
-        instrument.setType("bass guitar");
-        instrument.setBrand("Ibanez");
-        instrument.setManufacturer("Japan");
-        instrument.setNumberOfString (4);
-        instrument.setModel("GSR180 BK");
-        musShop.addInstrument(instrument);
+        Guitar guitar1 = new Guitar("Bass guitar", "Ibanez", "Japan", 4, "GSR180 BK", 6264 );
+        Guitar guitar2 = new Guitar("Electro guitar", "Gibson", "USA", 6, "LPMM142MSC1", 20331);
+        Guitar guitar3 = new Guitar("Acoustic guitar", "Fender", "USA", 6, "CD-60 ", 5211 );
+        Guitar guitar4 = new Guitar("Ukulele", "Ibanez", "Japan", 4, "UEW15E", 6264 );
 
-        Instrument instrument1 = new Instrument();
-        instrument1.setType("electro guitar");
-        instrument1.setBrand("Gibson");
-        instrument1.setManufacturer("USA");
-        instrument1.setNumberOfString (6);
-        instrument1.setModel("ES0335 TDC");
-        musShop.addInstrument(instrument);
+
+        Piano piano1 = new Piano("Yamaha", "Japan", 76, "DGX-230", 16050);
+        Piano piano2 = new Piano("Orla", "Italy", 88 , "Grand 310", 130828 );
+        Piano piano3 = new Piano("Casio", "Japan", 88, "PX-780BK", 26990);
+
+        Drum drum1 = new Drum("Acoustic Drums","Sonor", "Germany",   5, "SMF 11 Studio Set ", 17174);
+        Drum drum2 = new Drum("Djembe", "Maxtone", "China",  1, "ADJ08B ",  2664);
+        Drum drum3 = new Drum("Electro drums", "Roland", "Japan",  9, "TD-1KV", 18928);
+
+
+
+        /* Shop.addGuitar(guitar1);
+        Shop.addGuitar(guitar2);
+        Shop.addGuitar(guitar3);
+        Shop.addGuitar(guitar4);
+        System.out.println(Shop.toString());
+        */
+
 
         System.out.println(musShop.toString());
+
+
     }
 }
