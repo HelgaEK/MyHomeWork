1d8cc2db1f03acbfb03c649fce80e52c3879b017
Olga <mary.killer.ololo.yeah@gmail.com>
2016-03-08 16:34:42 +0200

    Object Guitar

---
+++ MusicShop/Guitar.java
@@ -0,0 +1,47 @@
+ public void setManufacturer(String manufacturer) {
+
+        this.manufacturer = manufacturer;
+    }
+
+    public int getNumberOfString() {
+
+        return numberOfstring;
+    }
+
+    public void setNumberOfString(int numberOfstring) {
+
+        this.numberOfstring = numberOfstring;
+    }
+
+    public String getModel() {
+
+        return model;
+    }
+
+    public void setModel(String model) {
+
+        this.model = model;
+    }
+    public int getPrice() {
+
+        return price;
+    }
+
+    public void setPrice(int price) {
+
+        this.price = price;
+    }
+
+    public Guitar(String type, String brand, String manufacturer, int numberOfstring, String model, int price) {
+        this.type = type;
+        this.brand = brand;
+        this.manufacturer = manufacturer;
+        this.numberOfstring = numberOfstring;
+        this.model = model;
+        this.price = price;
+    }
+
+    public String toString() {
+        return  "Type: " + this.type + "\nBrand: " + this.brand + "\nManufacturer: " + this.manufacturer + "\nNumber of String: " + this.numberOfstring + "\nModel: " + this.model + "\nPrice in hrivnia : " + this.price ;
+    }
+}
