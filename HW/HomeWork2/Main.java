87b5cc27676d7720898a2abf608d6ac37e343fec
Olga <mary.killer.ololo.yeah@gmail.com>
2016-03-20 21:31:40 +0200

    Create Main.java

---
+++ HW/HomeWork2/Main.java
@@ -0,0 +1,37 @@
+
+package HW1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Shop shop = new Shop();
+
+        shop.setName("City Music");
+        shop.setAddress("Shevchenko Boulevard,352");
+        shop.setNumberGuitarsOrder(1);
+
+        Guitar guitar = new Guitar();
+
+        guitar.setType("Electro guitar");
+        guitar.setBrand("Gibson");
+        guitar.setModel("LPMM142MSC1");
+        guitar.setManufacturer("USA");
+        guitar.setNumberOfString(6);
+        guitar.setPrice(20331);
+
+        guitar = new Guitar();
+
+        guitar.setType("Bass guitar");
+        guitar.setBrand("Ibanez");
+        guitar.setModel("GSR180 BK");
+        guitar.setManufacturer("Japan");
+        guitar.setNumberOfString(4);
+        guitar.setPrice(6264);
+
+        System.out.println(Shop.toString());
+
+        
+
+
+    }
+}
