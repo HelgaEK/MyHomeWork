9b2ff6d2ea6e61b98ed4f51e8cd4053d8d020778
Olga <mary.killer.ololo.yeah@gmail.com>
2016-05-11 08:49:01 +0300

    Update Drum.java

--- MusicShop/Drum.java
+++ MusicShop/Drum.java
@@ -1,41 +1,8 @@
-public class Drum {
-    protected String type;
-    protected String brand;
-    protected String manufacturer;
-    protected String model;
+public class Drum extends Guitar {
+    
     protected int numberOfdrum;
-    protected int price;
+   
 
-
-    public String getType() {
-
-        return type;
-    }
-
-    public void setType(String type) {
-
-        this.type = type;
-    }
-
-    public String getBrand() {
-
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-
-        this.brand = brand;
-    }
-
-    public String getManufacturer() {
-
-        return manufacturer;
-    }
-
-    public void setManufacturer(String manufacturer) {
-
-        this.manufacturer = manufacturer;
-    }
 
     public int getNumberOfDrum() {
 
@@ -47,31 +14,10 @@ public class Drum {
         this.numberOfdrum = numberOfdrum;
     }
 
-    public String getModel() {
-
-        return model;
-    }
-
-    public void setModel(String model) {
-
-        this.model = model;
-    }
-    public int getPrice() {
-
-        return price;
-    }
-
-    public void setPrice(int numberOfString) {
-
-        this.price = price;
-    }
-
-    public Drum(String type, String brand, String manufacturer, int numberOfString, String model, int price) {
-        this.type = type;
-        this.brand = brand;
-        this.manufacturer = manufacturer;
+    public Drum(String type, String brand, String manufacturer, int numberOfdrum, String model, int price) {
+        super(type, brand, manufacturer,model, price)
         this.numberOfdrum = numberOfdrum;
-        this.price = price;
+        
     }
 
     public String toString() {
